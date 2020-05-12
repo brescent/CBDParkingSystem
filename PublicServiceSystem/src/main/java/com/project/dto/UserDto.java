c917b7c01c3f04992984a1558e4cd71b74a4397e
69170 <691709434@qq.com>
2020-05-12 20:24:54 +0800

    公共服务实体类2.0

--- PublicServiceSystem/src/main/java/com/project/dto/UserDto.java
+++ PublicServiceSystem/src/main/java/com/project/dto/UserDto.java
@@ -1,25 +1,22 @@
 package com.project.dto;
 
-import javax.persistence.Column;
+
 import java.util.List;
 
-public class AdminUserDto {
+public class UserDto {
     private int id;
     private String loginName;
     private String pwd;
-    private int jobNum;
-    private String realName;
     private List<PowerDto> powerList;
 
-    public AdminUserDto() {
+
+    public UserDto() {
     }
 
-    public AdminUserDto(int id, String loginName, String pwd, int jobNum, String realName, List<PowerDto> powerList) {
+    public UserDto(int id, String loginName, String pwd, List<PowerDto> powerList) {
         this.id = id;
         this.loginName = loginName;
         this.pwd = pwd;
-        this.jobNum = jobNum;
-        this.realName = realName;
         this.powerList = powerList;
     }
 
@@ -45,22 +42,6 @@ public class AdminUserDto {
 
     public void setPwd(String pwd) {
         this.pwd = pwd;
-    }
-
-    public int getJobNum() {
-        return jobNum;
-    }
-
-    public void setJobNum(int jobNum) {
-        this.jobNum = jobNum;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
     }
 
     public List<PowerDto> getPowerList() {
