3cb12c3c33019bc065c0510cf47645436f289a3a
Brescent <826271694@qq.com>
2020-05-15 14:10:40 +0800

    白文楷 修改日志账单消息持久层

--- PublicServiceSystem/src/main/java/com/project/controller/BillController.java
+++ PublicServiceSystem/src/main/java/com/project/controller/BillController.java
@@ -3,8 +3,11 @@ package com.project.controller;
 import com.project.Service.IBillService;
 import com.project.Service.IUserService;
 import com.project.entity.BillEntity;
+import com.project.util.CBDStringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.Date;
@@ -25,8 +28,8 @@ public class BillController {
      * @param tradePrice 交易金额，正数为收入，负数为支出
      * @param billInfo 交易备注
      */
-    @RequestMapping("addBill/{userName}")
-    public void addBill(@PathVariable("userName") String userName, Double tradePrice, String billInfo){
+    @PostMapping("addBill/{userName}")
+    public String addBill(@PathVariable("userName") String userName, Double tradePrice, String billInfo){
         BillEntity billEntity = new BillEntity();
         if (tradePrice > 0){
             billEntity.setIncome(tradePrice);
@@ -36,10 +39,18 @@ public class BillController {
         billEntity.setUser(userService.findUserByName(userName));
         billEntity.setBillInfo(billInfo);
         billService.addBill(billEntity);
+
+        return CBDStringUtil.ADD_SUCCESS;
     }
 
-    @RequestMapping("findByItem")
-    public void findByItem(int userId, Date startDate, Date endDate){
+    @GetMapping("findByItem")
+    public List<BillEntity> findByItem(int userId, Date startDate, Date endDate){
         List<BillEntity> billList = billService.findByItem(userId,startDate,endDate);
+        return billList;
+    }
+
+    @GetMapping("findById/{billId}")
+    public BillEntity findById(@PathVariable("billId") int billId){
+        return billService.findById(billId);
     }
 }
--- PublicServiceSystem/src/main/java/com/project/util/CBDStringUtil.java
+++ PublicServiceSystem/src/main/java/com/project/util/CBDStringUtil.java
@@ -6,5 +6,6 @@ public class CBDStringUtil {
     public static final String ADMIN_USER="管理员";
     public static final String SUPERADMIN_USER="超级管理员";
     public static final String ADD_FAIL="当前用户名以存在，请换一个吧";
+    public static final String ADD_SUCCESS = "添加成功";
 
 }
