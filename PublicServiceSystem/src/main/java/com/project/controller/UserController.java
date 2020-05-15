e658a86ec5d7459e1af38cf6196a0a6e2f85a6f2
69170 <691709434@qq.com>
2020-05-15 14:33:25 +0800

    公共服务实体类2.0

--- PublicServiceSystem/src/main/java/com/project/controller/UserController.java
+++ PublicServiceSystem/src/main/java/com/project/controller/UserController.java
@@ -1,127 +1,167 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.project.controller;
 
 import com.project.Service.ILogService;
 import com.project.Service.IPowerService;
 import com.project.Service.IUserService;
+import com.project.dto.PersonalUserDto;
 import com.project.dto.PowerDto;
 import com.project.dto.UserDto;
-import com.project.entity.*;
+import com.project.entity.LogEntity;
+import com.project.entity.PersonalUserEntity;
+import com.project.entity.PowerEntity;
+import com.project.entity.PublicUserEntity;
 import com.project.util.CBDStringUtil;
 import com.project.util.MD5Util;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
-
     @Autowired
     IUserService service;
-
     @Autowired
     IPowerService powerService;
-
     @Autowired
     ILogService logService;
 
-    @RequestMapping("login/{userName}/{pwd}")
-    public UserDto login(@PathVariable("userName") String userName,@PathVariable("pwd") String pwd){
-        PublicUserEntity user=service.login(userName, pwd);
-        int userType=user.getUserType();
-        PublicUserEntity mainUser= service.findUserByName(userName);
-        UserDto userDto=new UserDto();
+    public UserController() {
+    }
+
+    @PostMapping({"login/{userName}/{pwd}"})
+    public UserDto login(@PathVariable("userName") String userName, @PathVariable("pwd") String pwd) {
+        PublicUserEntity user = this.service.login(userName, pwd);
+        int userType = user.getUserType();
+        PublicUserEntity mainUser = this.service.findUserByName(userName);
+        UserDto userDto = new UserDto();
+
         try {
-            //用户输入密码再次加盐再与数据库保存的密码进行比较，存在返回用户对象，不存在返回null
-            if(MD5Util.validPasswd(pwd,user.getPwd())){
-                //System.out.println("欢迎用户"+user.getLoginName()+"登陆系统");
-                //如果用户是管理员，查询权限
-                if(userType==2){
-                    List<PowerEntity> powerlist=powerService.getPowerByAdminId(mainUser.getId());
-                    List<PowerDto> powerDtoList=new ArrayList<>();
-                    for(PowerEntity power:powerlist){
-                        List<Integer> powerinfo= Collections.singletonList(power.getPowerInfo());
-                        //遍历拿到的数组，进行比较
-                        for (int i:powerinfo){
-                            if (i==1){
-                                String powerA="用户管理";
-                                powerDtoList.add(new PowerDto(powerA));
-                            }else if(i==2){
-                                String powerB="车位管理";
-                                powerDtoList.add(new PowerDto(powerB));
-                            }else if(i==3){
-                                String powerC="合同管理";
-                                powerDtoList.add(new PowerDto(powerC));
-                            }else if(i==4){
-                                String powerD="投诉管理";
-                                powerDtoList.add(new PowerDto(powerD));
-                            }
-                        }
-                        //设置UserDto的属性
-                        userDto.setPowerList(powerDtoList);
-                        userDto.setLoginName(mainUser.getLoginName());
-                        userDto.setId(mainUser.getId());
-                        userDto.setPwd(pwd);
-                        //判断用户属性，再进行设置用户的角色
-                        if (mainUser.getUserType()==0){
-                            userDto.setUserType(CBDStringUtil.PERSONAL_USER);
-                        }else if(mainUser.getUserType()==1){
-                            userDto.setUserType(CBDStringUtil.COMPANY_USER);
-                        }else if(mainUser.getUserType()==2){
-                            userDto.setUserType(CBDStringUtil.ADMIN_USER);
-                        }else if(mainUser.getUserType()==3){
-                            userDto.setUserType(CBDStringUtil.SUPERADMIN_USER);
+            if (!MD5Util.validPasswd(pwd, user.getPwd())) {
+                return null;
+            }
+
+            if (userType == 2) {
+                List<PowerEntity> powerlist = this.powerService.getPowerByAdminId(mainUser.getId());
+                List<PowerDto> powerDtoList = new ArrayList();
+                Iterator var9 = powerlist.iterator();
+
+                while(var9.hasNext()) {
+                    PowerEntity power = (PowerEntity)var9.next();
+                    List<Integer> powerinfo = Collections.singletonList(power.getPowerInfo());
+                    Iterator var12 = powerinfo.iterator();
+
+                    while(var12.hasNext()) {
+                        int i = (Integer)var12.next();
+                        String powerD;
+                        if (i == 1) {
+                            powerD = "用户管理";
+                            powerDtoList.add(new PowerDto(powerD));
+                        } else if (i == 2) {
+                            powerD = "车位管理";
+                            powerDtoList.add(new PowerDto(powerD));
+                        } else if (i == 3) {
+                            powerD = "合同管理";
+                            powerDtoList.add(new PowerDto(powerD));
+                        } else if (i == 4) {
+                            powerD = "投诉管理";
+                            powerDtoList.add(new PowerDto(powerD));
                         }
                     }
 
-                   LogEntity logEntity= new LogEntity();
-                    logService.addLog(logEntity);
+                    userDto.setPowerList(powerDtoList);
+                    userDto.setLoginName(mainUser.getLoginName());
+                    userDto.setId(mainUser.getId());
+                    userDto.setPwd(pwd);
+                    if (mainUser.getUserType() == 0) {
+                        userDto.setUserType(CBDStringUtil.PERSONAL_USER);
+                    } else if (mainUser.getUserType() == 1) {
+                        userDto.setUserType(CBDStringUtil.COMPANY_USER);
+                    } else if (mainUser.getUserType() == 2) {
+                        userDto.setUserType(CBDStringUtil.ADMIN_USER);
+                    } else if (mainUser.getUserType() == 3) {
+                        userDto.setUserType(CBDStringUtil.SUPERADMIN_USER);
+                    }
                 }
-            }else {
-                    return null;
+
+                LogEntity log = new LogEntity(userName,CBDStringUtil.LOGIN_LOG);
+                this.logService.addLog(log);
             }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        } catch (NoSuchAlgorithmException var15) {
+            var15.printStackTrace();
+        } catch (UnsupportedEncodingException var16) {
+            var16.printStackTrace();
         }
+
         return userDto;
     }
 
-
-    @RequestMapping("addUser")
-    public String addUser(PublicUserEntity publicUserEntity,PersonalUserEntity personalUserEntity){
-        //通过姓名查找用户，如果用户存在，返回false
-            PublicUserEntity mainUser=service.findUserByName(publicUserEntity.getLoginName());
-            if(mainUser!=null){
-                try {
-                    //盐加密输入的密码，再将输入的密码保存到数据库
-                    String  encryptedPwd = MD5Util.getEncryptedPwd(publicUserEntity.getPwd());
-                    publicUserEntity.setPwd(encryptedPwd);
-                    service.addUser(publicUserEntity, personalUserEntity);
-                    return "";
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-
+    @RequestMapping({"addUser"})
+    public String addUser(PublicUserEntity publicUserEntity, PersonalUserEntity personalUserEntity) {
+        PublicUserEntity mainUser = this.service.findUserByName(publicUserEntity.getLoginName());
+        if (mainUser != null) {
+            try {
+                String encryptedPwd = MD5Util.getEncryptedPwd(publicUserEntity.getPwd());
+                publicUserEntity.setPwd(encryptedPwd);
+                this.service.addUser(publicUserEntity, personalUserEntity);
+                LogEntity log = new LogEntity(publicUserEntity.getLoginName(), CBDStringUtil.ADDUSER_LOG);
+                this.logService.addLog(log);
+            } catch (NoSuchAlgorithmException var6) {
+                var6.printStackTrace();
+            } catch (UnsupportedEncodingException var7) {
+                var7.printStackTrace();
             }
-            return CBDStringUtil.ADD_FAIL;
+        }
+
+        return CBDStringUtil.ADD_FAIL;
     }
 
+    @GetMapping({"getPersonalUserInfo/{userId}"})
+    public PersonalUserDto getPersonalUserInfo(@PathVariable("userId") int userId) {
+        PersonalUserEntity user = this.service.findByPublicUserId(userId);
+        PersonalUserDto userDto = new PersonalUserDto();
+        userDto.setEmail(user.getEmail());
+        userDto.setHomeAddress(user.getHomeAddress());
+        userDto.setPhone(user.getPhone());
+        userDto.setPhone(user.getPhone());
+        return userDto;
+    }
 
+    @RequestMapping({"updateUser"})
+    public void updatePersonalUser(String userName, PersonalUserDto personalUserDto) {
+        PublicUserEntity maiuUser = this.service.findUserByName(userName);
+        PersonalUserEntity user = this.service.findByPublicUserId(maiuUser.getId());
+        user.setPhone(personalUserDto.getPhone());
+        user.setJobInfo(personalUserDto.getJonInfo());
+        user.setEmail(personalUserDto.getEmail());
+        user.setHomeAddress(personalUserDto.getHomeAddress());
+        String encryptedPwd = null;
 
-    @RequestMapping("updateUser/{email}/{jobInfo}/{phone}/{homeAddress}/{pwd}/{userId}")
-    public void updatePersonalUser(@PathVariable("email") String email,@PathVariable("jobInfo") String jobInfo,@PathVariable("phone") String phone,@PathVariable("homeAddress") String homeAddress,@PathVariable("userName") String userName,@PathVariable("pwd") String pwd,@PathVariable("userId") int userId){
+        try {
+            encryptedPwd = MD5Util.getEncryptedPwd(personalUserDto.getPwd());
+            maiuUser.setPwd(encryptedPwd);
+        } catch (NoSuchAlgorithmException var7) {
+            var7.printStackTrace();
+        } catch (UnsupportedEncodingException var8) {
+            var8.printStackTrace();
+        }
 
+        LogEntity log = new LogEntity(userName,CBDStringUtil.UPDATEUSER_LOG);
+        this.logService.addLog(log);
+        this.service.updatePersonalUser(user, maiuUser.getId(), maiuUser);
     }
 }
--- PublicServiceSystem/src/main/java/com/project/util/CBDStringUtil.java
+++ PublicServiceSystem/src/main/java/com/project/util/CBDStringUtil.java
@@ -1,11 +1,12 @@
 package com.project.util;
 
 public class CBDStringUtil {
-    public static final String PERSONAL_USER="个人用户";
-    public static final String COMPANY_USER="企业用户";
-    public static final String ADMIN_USER="管理员";
-    public static final String SUPERADMIN_USER="超级管理员";
-    public static final String ADD_FAIL="当前用户名以存在，请换一个吧";
-    public static final String ADD_SUCCESS = "添加成功";
-
+    public static final String PERSONAL_USER = "个人用户";
+    public static final String COMPANY_USER = "企业用户";
+    public static final String ADMIN_USER = "管理员";
+    public static final String SUPERADMIN_USER = "超级管理员";
+    public static final String ADD_FAIL = "当前用户名以存在，请换一个吧";
+    public static final String LOGIN_LOG = "登陆系统";
+    public static final String ADDUSER_LOG = "添加用户";
+    public static final String UPDATEUSER_LOG = "修改了个人信息";
 }
