9f0d7dc7eee6c67c146e45d8892712f7174da33d
ratelMan <1445401032@qq.com>
2020-05-22 09:50:51 +0800

    后台  dto

---
+++ CBDWeb/src/main/java/com/project/dto/CompanyContractDto.java
@@ -0,0 +1,168 @@
+package com.project.dto;
+
+import com.lovo.back.entity.CompanyContractAndStall;
+import com.project.entity.CompanyUserEntity;
+
+import javax.persistence.Column;
+import java.sql.Date;
+import java.util.Optional;
+import java.util.Set;
+
+public class CompanyContractDto {
+
+
+
+    /*企业合同主键 唯一标志*/
+
+    private int id;
+    /*企业合同编号*/
+
+    private String contractNo;
+    /*合同生效时期*/
+
+    private Date startDate;
+    /*合同失效时间*/
+
+    private Date endDate;
+    /*合同成交价格*/
+
+    private double dealPrice;
+
+    /*合同图片名称*/
+
+    private String contractImg;
+    /*旧合同编号  可为空为续约合同  准备*/
+
+    private String oldContractNo;
+
+
+    private  String  companyName;
+
+
+    private String linkMan;
+
+    private String phone;
+
+    private Set<CompanyContractAndStall> set;
+    /*车位*/
+
+    /*合同生效状态*/
+    private int state=1;
+
+    public CompanyContractDto() {
+    }
+
+    public CompanyContractDto(String contractNo, Date startDate, Date endDate, double dealPrice, String contractImg, String oldContractNo) {
+        this.contractNo = contractNo;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.dealPrice = dealPrice;
+        this.contractImg = contractImg;
+        this.oldContractNo = oldContractNo;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContractNo() {
+        return contractNo;
+    }
+
+    public void setContractNo(String contractNo) {
+        this.contractNo = contractNo;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getDealPrice() {
+        return dealPrice;
+    }
+
+    public void setDealPrice(double dealPrice) {
+        this.dealPrice = dealPrice;
+    }
+
+    public String getContractImg() {
+        return contractImg;
+    }
+
+    public void setContractImg(String contractImg) {
+        this.contractImg = contractImg;
+    }
+
+    public String getOldContractNo() {
+        return oldContractNo;
+    }
+
+    public void setOldContractNo(String oldContractNo) {
+        this.oldContractNo = oldContractNo;
+    }
+
+//    public void setCompany(CompanyEntity company) {
+//        this.company = company;
+//    }
+
+
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public Set<CompanyContractAndStall> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<CompanyContractAndStall> set) {
+        this.set = set;
+    }
+
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getLinkMan() {
+        return linkMan;
+    }
+
+    public void setLinkMan(String linkMan) {
+        this.linkMan = linkMan;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
